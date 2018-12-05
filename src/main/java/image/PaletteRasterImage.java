2ef976a8605bb60ee4ce1c8db347c205aa5e1952
belHak <hak721@outlook.fr>
2018-12-05 11:09:30 +0100

    correct setPixel

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -2,28 +2,21 @@ package image;
 
 import javafx.scene.paint.Color;
 
+import java.util.List;
+
 public class PaletteRasterImage implements Image {
 
-    private Color[] color = {Color.blue, Color.white , Color.red} ;
+
+    List<Color> palette ;
     private int height , width ;
-    private int pixels[][] ;
+    private int[][] indexesOfColors ;
 
     public PaletteRasterImage(Color color, int width, int height){
-
+        int colorNumber = 0 ;
         this.width = width ;
         this.height = height ;
-        pixels = new int[width][height];
-        int pixelColor = 0 ;
-        for(int index = 0 ; index < 3 ; index ++){
-            if(color == this.color[index]) {
-                pixelColor = index;
-            }
-        }
-        for(int x = 0 ;x < width ; x++){
-            for(int y = 0 ; y < height ; y++ ){
-                pixels[x][y] = pixelColor ;
-            }
-        }
+        indexesOfColors = new int[width][height];
+
 
     }
     public PaletteRasterImage(Color[][] pixels){
@@ -39,33 +32,29 @@ public class PaletteRasterImage implements Image {
 
     }
     public void setPixelColor(Color color, int x, int y){
-
-        int pixelColor = 0 ;
-
-            if(color == Color.red)
-                pixelColor = 2 ;
-            if(color == Color.white)
-                pixelColor = 1 ;
-            if(color == color.blue)
-                pixelColor = 0 ;
-
-        pixels[x][y] = pixelColor ;
+        if(!palette.contains(color))
+            palette.add(color);
+        indexesOfColors[x][y] = palette.indexOf(color);
     }
+    
     public void createRepresentation(){
-        pixels = new int[width][height];
+        indexesOfColors = new int[width][height];
 
     }
     public Color getPixelColor(int x, int y){
 
         for(int index = 0 ; index < 3 ; index ++){
-            if(pixels[x][y] == index) {
+            if(indexesOfColors[x][y] == index) {
                 return color[index] ;
 
             }
         }
 
             throw new ArithmeticException("/ aucune couleur");
-
         }
+    public void setPixelsColor(Color[][] pixels){
+        int numberColor = 0 ;
+        for(int index = 0 ; index < 3 ; index ++)
+    }
 
 }
