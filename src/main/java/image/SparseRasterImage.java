9ab4c7d87b0000e81cf6c93f2092f126f001d160
belHak <hak721@outlook.fr>
2018-12-07 10:36:23 +0100

    premier constructeur

--- src/main/java/image/SparseRasterImage.java
+++ src/main/java/image/SparseRasterImage.java
@@ -1,11 +1,26 @@
 package image;
 import javafx.scene.paint.Color;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class SparseRasterImage extends RasterImage implements Image {
 
     private Map<Point,Color> pixelMap ;
-    private int[][] pixels ;
+    private Point[][] pixels ;
 
-    
+    public SparseRasterImage(Color color, int width, int height){
+        super(width, height);
+        this.createRepresentation();
+        for(int x = 0 ;x < width ; x++ ){
+            for(int y = 0 ; y < height ; y++){
+                pixelMap.put(pixels[x][y],color);
+            }
+        }
+    }
+    public void createRepresentation(){
+        pixels = new Point[width][height];
+        pixelMap = new HashMap<Point, Color>();
+
+    }
 }
