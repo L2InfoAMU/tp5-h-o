de180307ea9c1bfc2e738b0a1ac03d50f6a7cc3f
belHak <hak721@outlook.fr>
2018-12-07 10:22:28 +0100

    Initialisation SparseRasterImage

---
+++ src/main/java/image/SparseRasterImage.java
@@ -0,0 +1,11 @@
+package image;
+import javafx.scene.paint.Color;
+import java.util.Map;
+
+public class SparseRasterImage extends RasterImage implements Image {
+
+    private Map<Point,Color> pixelMap ;
+    private int[][] pixels ;
+
+    
+}
