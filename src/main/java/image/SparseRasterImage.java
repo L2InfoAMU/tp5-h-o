7162a1869bc5c929c4815f73f97e93bae16e58d4
belHak <hak721@outlook.fr>
2018-12-07 10:38:47 +0100

    deuxieme constructeur

--- src/main/java/image/SparseRasterImage.java
+++ src/main/java/image/SparseRasterImage.java
@@ -18,9 +18,20 @@ public class SparseRasterImage extends RasterImage implements Image {
             }
         }
     }
+    public SparseRasterImage(Color[][] pixels){
+        super(pixels.length,pixels[0].length);
+        this.createRepresentation();
+        for(int x = 0 ;x < width ; x++ ){
+            for(int y = 0 ; y < height ; y++){
+                pixelMap.put(this.pixels[x][y],pixels[x][y]);
+            }
+        }
+
+    }
     public void createRepresentation(){
         pixels = new Point[width][height];
         pixelMap = new HashMap<Point, Color>();
 
     }
+    
 }
