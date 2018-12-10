e128cfcb458200b5c31b2f04b0ff62f49a73a1f3
belHak <hak721@outlook.fr>
2018-12-10 10:52:39 +0100

    implémentation rectangle

---
+++ src/main/java/image/Rectangle.java
@@ -0,0 +1,23 @@
+package image;
+import javafx.scene.paint.Color;
+
+public class Rectangle implements Shape {
+
+    int x,y ;
+    Color[][] pixelColor ;
+    Rectangle(int x, int y, int width, int height, Color color){
+        this.x = x;
+        this.y = y ;
+        pixelColor = new Color[width][height];
+        for(int wid = 0 ; wid < width ; wid++){
+            for(int hgt = 0; hgt < height ;hgt++){
+                pixelColor[wid][hgt]= color ;
+            }
+        }
+    }
+    public boolean contains(Point point){
+        if(point.x > pixelColor.length || point.y > pixelColor[0].length)
+            return false ;
+        return true ;
+    }
+}
