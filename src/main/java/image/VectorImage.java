23d4cd6084cc79ed31d8925b9c58332aa7b5e4f0
belHak <hak721@outlook.fr>
2018-12-10 11:20:32 +0100

    implémentation VectorImage

---
+++ src/main/java/image/VectorImage.java
@@ -0,0 +1,30 @@
+package image;
+
+import javafx.scene.paint.Color;
+
+import java.util.List;
+
+public class VectorImage implements Image{
+
+    List<Shape> shapes ;
+    Color[][] pixelColor ;
+
+    VectorImage(List<Shape> shapes, int width, int height){
+        int size = shapes.size();
+        this.shapes = shapes ;
+        pixelColor = new Color[width][height];
+        for(int index = 0 ; index < size ; index ++){
+            for(int x = 0 ; x < width ; x++){
+                for(int y = 0 ; y < height ; y++){
+                    if(shapes.get(size).contains(new Point(x,y)))
+                        pixelColor[x][y] = shapes.get(size).getColor();
+                }
+            }
+        }
+    }
+    public Color getPixelColor(int x, int y){
+        return pixelColor[x][y] ;
+    }
+
+
+}
